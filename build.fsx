55eb2a4a87163cdc683de4c1a5527b8f50d57db6
Timothé Larivière <timothe.lariviere@outlook.com>
2019-02-19 11:36:21 +0100

    .

--- build.fsx
+++ build.fsx
@@ -168,7 +168,7 @@ Target.create "RunTests" (fun _ ->
     let setDotNetOptions (options: DotNet.TestOptions) =
         { options with
               TestAdapterPath = Some "."
-              Logger = Some "trx"
+              Logger = Some "xml"
               ResultsDirectory = Some (Path.Combine(buildDir, "TestResults")) }
 
     let testProjects = !! "tests/**/*.fsproj"
